b6b8efcce96b6ab15d57a0cf088fc8e5539b39b9
Joan Yassi <joan@tray.io>
2022-07-24 23:22:15 +0100

    added teh main components

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,21 +2,21 @@ import { Card, CardHeader, CardContent, Typography } from '@mui/material';
 import { Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles';
-import useTransactions from '../../useTransactions';
+// import useTransactions from '../../useTransactions';
 
-const DetailsCard = ({ title, subheader }) => {
-  const { total, chartData } = useTransactions(title);
-  const { income, expense } = useStyles();
+const Details = ({ title, subheader }) => {
+  // const { total, chartData } = useTransactions(title);
+  const classes = useStyles();
 
   return (
-    <Card className={title === 'Income' ? income : expense}>
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
       <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant="h5">${total}</Typography>
-        <Doughnut data={chartData} />
+        <Typography variant="h5">${}</Typography>
+        {/* <Doughnut /> */}
       </CardContent>
     </Card>
   );
 };
 
-export default DetailsCard;
+export default Details;
