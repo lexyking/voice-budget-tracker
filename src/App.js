dbcc94b745d938763a69d81ff197a1bf3ec04a3d
Joan Yassi <joan@tray.io>
2022-07-24 23:12:58 +0100

    Updated the design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import './index.css'
+import { Grid } from '@mui/material';
+import { useRef } from 'react';
+
+import Details from './components/Details/Details'
+import useStyles from './styles';
+
 
 const App = () => {
+  const classes = useStyles();
+  const main = useRef(null)
   return (
-    <div>Hello from app</div>
+    <div>
+    <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+      <Grid item xs={12} sm={4} className={classes.mobile}>
+        <Details title="Income" />
+      </Grid>
+      <Grid ref={main} item xs={12} sm={3} className={classes.main}>
+        {/* <Main /> */}
+      </Grid>
+      <Grid item xs={12} sm={4} className={classes.desktop}>
+        <Details title="Income" />
+      </Grid>
+      <Grid item xs={12} sm={4} className={classes.last}>
+        <Details title="Expense" />
+      </Grid>
+      {/* <PushToTalkButtonContainer>
+        <PushToTalkButton />
+      </PushToTalkButtonContainer> */}
+    </Grid>
+  </div>
   )
 }
 
